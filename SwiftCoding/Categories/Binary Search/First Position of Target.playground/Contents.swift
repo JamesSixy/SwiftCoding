0631fb12f2dc3d333ea06e8bb3d02f624448ecf6
Hao Zheng <babyshung8@gmail.com>
2017-02-17 19:02:22 -0500

    First Position of Target

--- SwiftCoding/Categories/Binary Search/First Position of Target.playground/Contents.swift
+++ SwiftCoding/Categories/Binary Search/First Position of Target.playground/Contents.swift
@@ -1,6 +1,7 @@
 
-///  Last Position of Target
-///  http://www.lintcode.com/en/problem/last-position-of-target/
+/// First Position of Target
+/// http://www.lintcode.com/en/problem/first-position-of-target/
+///
 /// - Parameters:
 ///   - nums: <#nums description#>
 ///   - target: <#target description#>
@@ -8,24 +9,26 @@
 
 import UIKit
 
-func lastPosition(nums: [Int], target: Int) -> Int {
+func firstPosition(nums: [Int], target: Int) -> Int {
     guard nums.count > 0 else {
         return -1
     }
     var start = 0, end = nums.count - 1
     while start + 1 < end {
         let mid = start + (end - start) / 2
-        if nums[mid] <= target {
-            start = mid
-        } else {
+        if nums[mid] >= target {
             end = mid
+        } else {
+            start = mid
         }
     }
-    if nums[end] == target {
-        return end
-    } else if nums[start] == target {
+    if nums[start] == target {
         return start
+    } else if nums[end] == target {
+        return end
     } else {
         return -1
     }
 }
+
+//firstPosition(nums: [1, 2, 3, 3, 4, 5, 10], target: 3)
