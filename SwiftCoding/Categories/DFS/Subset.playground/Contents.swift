fa4d72a654fd1a1ff393c732d32f52fa24dde7a3
Hao Zheng <babyshung8@gmail.com>
2017-02-23 21:32:29 -0500

    [DFS]90. Subsets II

--- SwiftCoding/Categories/DFS/Subset.playground/Contents.swift
+++ SwiftCoding/Categories/DFS/Subset.playground/Contents.swift
@@ -27,3 +27,39 @@ func subsetHelper(_ res: inout [[Int]],
         container.removeLast()
     }
 }
+
+
+/// 90. Subsets II
+/// https://leetcode.com/problems/subsets-ii/?tab=Description
+/// - Parameter nums: <#nums description#>
+/// - Returns: <#return value description#>
+
+func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
+    var res: [[Int]] = []
+    guard nums.count > 0 else {
+        return res
+    }
+    var container: [Int] = []
+    subsetDupHelper(&res, &container, 0, nums.sorted())
+    return res
+}
+
+func subsetDupHelper(_ res: inout [[Int]],
+                  _ container: inout [Int],
+                  _ start: Int,
+                  _ nums: [Int]) {
+    res.append(container)
+    for i in stride(from: start, to: nums.count, by: 1) {
+        if i != start && nums[i] == nums[i - 1] {
+            continue
+        }
+        container.append(nums[i])
+        subsetDupHelper(&res, &container, i + 1, nums)
+        container.removeLast()
+    }
+}
+
+
+
+
+
