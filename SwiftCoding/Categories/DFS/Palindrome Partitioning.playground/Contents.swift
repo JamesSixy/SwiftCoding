6e33dd25a1d667a1555f26471a855650f40f1dc0
Hao Zheng <babyshung8@gmail.com>
2017-02-25 18:35:36 -0500

    [DFS]added questions

---
+++ SwiftCoding/Categories/DFS/Palindrome Partitioning.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+/// 131. Palindrome Partitioning
+/// https://leetcode.com/problems/palindrome-partitioning/?tab=Description
+/// - Parameter s: <#s description#>
+/// - Returns: <#return value description#>
+
+func partition(_ s: String) -> [[String]] {
+    
+}
---
+++ SwiftCoding/Categories/DFS/Word Squares.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+
+/// 425. Word Squares
+/// https://leetcode.com/problems/word-squares/?tab=Description
+/// - Parameter words: <#words description#>
+/// - Returns: <#return value description#>
+func wordSquares(_ words: [String]) -> [[String]] {
+    
+}
+
+
+
+/// 422. Valid Word Square
+/// https://leetcode.com/problems/valid-word-square/?tab=Description
+/// - Parameter words: <#words description#>
+/// - Returns: <#return value description#>
+
+func validWordSquare(_ words: [String]) -> Bool {
+    
+}
