6e4b1406cbd8fe15206a7758a66343cfc9fc7e62
BabyShung <babyshung8@gmail.com>
2017-02-26 16:16:40 -0500

    [Stack]94. Binary Tree Inorder Traversal

--- SwiftCoding/Categories/Stack/Binary Tree Iterative Traversals.playground/Contents.swift
+++ SwiftCoding/Categories/Stack/Binary Tree Iterative Traversals.playground/Contents.swift
@@ -7,5 +7,18 @@ import ZHDataStructure
 /// - Returns: <#return value description#>
 
 func inorderTraversal(_ root: TreeNode?) -> [Int] {
-    return []
+    var res: [Int] = []
+    guard let root = root else { return res }
+    var cur: TreeNode? = root
+    var stack: [TreeNode] = []
+    while !stack.isEmpty || cur != nil {
+        while cur != nil {
+            stack.append(cur!) //push
+            cur = cur!.left
+        }
+        cur = stack.removeLast() //pop
+        res.append(cur!.val)
+        cur = cur!.right
+    }
+    return res
 }
