15e7b05f0f4120d515328696992ae9ee85ba547b
Hao Zheng <babyshung8@gmail.com>
2017-02-26 14:23:36 -0500

    [BFS]added question

---
+++ SwiftCoding/Categories/String/BFS/Number of Islands BFS.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+/// 200. Number of Islands
+/// https://leetcode.com/problems/number-of-islands/?tab=Description
+/// - Parameter grid: <#grid description#>
+/// - Returns: <#return value description#>
+
+func numIslands(_ grid: [[Character]]) -> Int {
+    return 0
+}
