fa8ad13dfcafb1cfc0156a80866b799b36fcfe8a
BabyShung <babyshung8@gmail.com>
2017-02-26 16:59:09 -0500

    [Stack]Longest Valid Parentheses

--- SwiftCoding/Categories/Stack/Longest Valid Parentheses.playground/Contents.swift
+++ SwiftCoding/Categories/Stack/Longest Valid Parentheses.playground/Contents.swift
@@ -3,6 +3,33 @@
 /// https://leetcode.com/problems/longest-valid-parentheses/?tab=Description
 /// - Parameter s: <#s description#>
 /// - Returns: <#return value description#>
+
 func longestValidParentheses(_ s: String) -> Int {
-    return 0
+    let arr = [Character](s.characters)
+    guard arr.count > 0 else { return 0 }
+    var stack: [Int] = []
+    var start = 0, maxLen = 0
+    for (i, char) in arr.enumerated() {
+        if char == "(" {
+            stack.append(i) //push
+        } else {
+            if stack.isEmpty {
+                start = i + 1
+            } else {
+                stack.removeLast() //pop
+                if stack.isEmpty {
+                    maxLen = max(i - start + 1, maxLen)
+                } else {
+                    maxLen = max(i - stack.last!, maxLen)
+                }
+            }
+        }
+    }
+    return maxLen
 }
+
+//let testCases = ["", "(", ")", "()", ")(", "((((", "))))",
+//                  ")()(", "()(", ")()", "()()", "((((()))(()"]
+//for str in testCases {
+//    print(longestValidParentheses(str))
+//}
