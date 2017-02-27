a2809725c4bcb48670c0f9bd3122873935572b89
BabyShung <babyshung8@gmail.com>
2017-02-27 08:33:00 -0500

    [Stack]402. Remove K Digits

--- SwiftCoding/Categories/Stack/Remove K Digits.playground/Contents.swift
+++ SwiftCoding/Categories/Stack/Remove K Digits.playground/Contents.swift
@@ -9,5 +9,38 @@ import ZHDataStructure
 /// - Returns: <#return value description#>
 
 func removeKdigits(_ num: String, _ k: Int) -> String {
-    return ""
+    var stack = [Character](), k = k
+    let chars = num.characters, size = chars.count - k
+    
+    for char in chars {
+        while k > 0 &&
+            stack.count > 0 &&
+            charToInt(char) < charToInt(stack.last!) {
+            stack.removeLast()
+            k -= 1
+        }
+        stack.append(char)
+        print("\(stack), k =\(k)")
+    }
+    
+    stack = Array(stack[0 ..< size])
+    for char in stack {
+        if char != "0" {
+            break
+        } else {
+            stack.removeFirst()
+        }
+    }
+    
+    return stack.isEmpty ? "0" : String(stack)
 }
+
+private func charToInt(_ c: Character) -> Int {
+    return Int(String(c))!
+}
+
+//let testCases = [("9988776655", 3), ("1000082", 2)]
+//for tuple in testCases {
+//    print(removeKdigits(tuple.0, tuple.1))
+//}
+
