5c46a1e8a3756f7db540bb24a8d276047f2a0f2f
Hao Zheng <babyshung8@gmail.com>
2017-02-27 21:15:00 -0500

    WTF word break
    waste my time

--- SwiftCoding/Categories/DFS/Word Break I.playground/Contents.swift
+++ SwiftCoding/Categories/DFS/Word Break I.playground/Contents.swift
@@ -1,6 +1,31 @@
 
 import UIKit
 
+/// 139. Word Break
+/// https://leetcode.com/problems/word-break/?tab=Description
+/// - Parameters:
+///   - s: <#s description#>
+///   - wordDict: <#wordDict description#>
+/// - Returns: <#return value description#>
+
+func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+    return helper(s, Set(wordDict), 0)
+}
+
+private func helper(_ s: String,
+                    _ set: Set<String>,
+                    _ start: Int) -> Bool {
+    if start == s.characters.count {
+        return true
+    }
+    var res = false
+    for i in stride(from: start + 1, through: s.characters.count, by: 1) {
+        if set.contains(s[start ..< i]) {
+            res = res || helper(s, set, i)
+        }
+    }
+    return res
+}
 
 extension String {
     
@@ -29,36 +54,8 @@ extension String {
     }
 }
 
-
-/// 139. Word Break
-/// https://leetcode.com/problems/word-break/?tab=Description
-/// - Parameters:
-///   - s: <#s description#>
-///   - wordDict: <#wordDict description#>
-/// - Returns: <#return value description#>
-
-func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
-    return helper(s, Set(wordDict), 0)
-}
-
-private func helper(_ s: String,
-                    _ set: Set<String>,
-                    _ start: Int) -> Bool {
-    if start == s.characters.count {
-        return true
-    }
-    var res = false
-    for i in stride(from: start + 1, through: s.characters.count, by: 1) {
-        if set.contains(s[start ..< i]) {
-            res = res || helper(s, set, i)
-        }
-    }
-    return res
-}
-
-let testS = ["leetcode", "lintcode", "laddercode", "lescode", "codelint"]
-let testD = ["leet", "code", "lint"]
-for str in testS {
-    print(wordBreak(str, testD))
-}
-
+//let testS = ["leetcode", "lintcode", "laddercode", "lescode", "codelint"]
+//let testD = ["leet", "code", "lint"]
+//for str in testS {
+//    print(wordBreak(str, testD))
+//}
--- SwiftCoding/Categories/DFS/Word Break II.playground/Contents.swift
+++ SwiftCoding/Categories/DFS/Word Break II.playground/Contents.swift
@@ -1,5 +1,42 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+/// 140. Word Break II
+/// https://leetcode.com/problems/word-break-ii/?tab=Description
+/// - Parameters:
+///   - s: <#s description#>
+///   - wordDict: <#wordDict description#>
+/// - Returns: <#return value description#>
 
-var str = "Hello, playground"
+//func wordBreak(_ s: String, _ wordDict: [String]) -> [String] {
+//    var res = [String]()
+//    let arr = [Character](s.characters)
+//    guard arr.count > 0 else { return res }
+//    var container: [Character] = []
+//    wbHelper(&res, &container, arr, Set(wordDict), 0)
+//    return res
+//}
+//
+//private func wbHelper(_ res: inout [String],
+//                      _ container: inout [Character],
+//                      _ arr: [Character],
+//                      _ set: Set<String>,
+//                      _ start: Int) {
+//    if start == arr.count {
+//        res.append(String(container))
+//        return
+//    }
+//    for i in stride(from: start, to: arr.count, by: 1) {
+//        let sub = String(Array(arr[start ..< i + 1]))
+//        if set.contains(sub) {
+//            var subLen = sub.characters.count
+//            if container.count != 0 {
+//                container.append(" ")
+//                subLen += 1
+//            }
+//            container += [Character](sub.characters)
+//            wbHelper(&res, &container, arr, set, container.count)
+//            container.removeLast(subLen)
+//        }
+//    }
+//}
+
+
