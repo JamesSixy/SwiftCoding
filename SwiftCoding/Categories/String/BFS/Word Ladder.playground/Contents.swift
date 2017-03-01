5d0dbc1828a478b702667e39d4caa22deafd685d
Hao Zheng <babyshung8@gmail.com>
2017-03-01 09:19:09 -0500

    [BFS]Temp

--- SwiftCoding/Categories/String/BFS/Word Ladder.playground/Contents.swift
+++ SwiftCoding/Categories/String/BFS/Word Ladder.playground/Contents.swift
@@ -1,4 +1,6 @@
 
+import UIKit
+
 /// 127. Word Ladder
 /// https://leetcode.com/problems/word-ladder/?tab=Description
 /// - Parameters:
@@ -9,4 +11,36 @@
 
 func ladderLength(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
     
+    guard beginWord != endWord,
+        wordList.count > 0 else { return 0 }
+
+    let set = Set(wordList)
+    var visited = [String : Int]()
+    var queue = [beginWord]
+    visited[beginWord] = 0
+    
+    while !queue.isEmpty {
+        let cur = queue.removeFirst() //dequeue
+        for word in transformedWordsInDict(beginWord, set) {
+            if let word = visited[word] {
+            }
+        }
+    }
+    return 0
+}
+
+private func transformedWordsInDict(_ beginWord: String, _ set: Set<String>) -> [String] {
+    var res = [String]()
+    let letters = (0 ..< 26).map { Character(UnicodeScalar($0)) }
+    for i in letters {
+        for (index, _) in beginWord.characters.enumerated() {
+            var replacedArr = Array(beginWord.characters)
+            replacedArr[index] = i
+            let str = String(replacedArr)
+            if set.contains(str) {
+                res.append(str)
+            }
+        }
+    }
+    return res
 }
