bbd337378416c076b668d534979856a7b34600e2
BabyShung <babyshung8@gmail.com>
2017-02-28 23:10:51 -0500

    [BFS]207. Course Schedule

--- SwiftCoding/Categories/String/BFS/Course Schedule.playground/Contents.swift
+++ SwiftCoding/Categories/String/BFS/Course Schedule.playground/Contents.swift
@@ -7,10 +7,54 @@
 /// - Returns: <#return value description#>
 
 func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
-    
+
+    var indegree = getInDegree(numCourses, prerequisites)
+    let edges = getEdges(numCourses, prerequisites)
+
+    var queue = indegree.enumerated().filter {
+        $0.element == 0
+        }.map { $0.offset }
+    var count = 0
+    while !queue.isEmpty {
+        let cur = queue.removeFirst()
+        count += 1
+        for neighbor in edges[cur] {
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0 {
+                queue.append(neighbor)
+            }
+        }
+    }
+    return count == numCourses
 }
 
-
-func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
-    
+private func getInDegree(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+    var res = Array(repeatElement(0, count: numCourses))
+    for i in 0 ..< prerequisites.count {
+        let course = prerequisites[i][0]
+        res[course] += 1
+    }
+    return res
 }
+
+private func getEdges(_ numCourses: Int, _ prerequisites: [[Int]]) -> [[Int]] {
+    var res = Array(repeatElement([Int](), count: numCourses))
+    for i in 0 ..< prerequisites.count {
+        let neighbor = prerequisites[i][1]
+        res[neighbor].append(prerequisites[i][0])
+    }
+    return res
+}
+
+let testCases = [
+    (2, []),
+    (2, [[0, 1]])
+]
+//** TEST
+//for tuple in testCases {
+//    print(canFinish(tuple.0, tuple.1))
+//}
+
+//func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+//    
+//}
