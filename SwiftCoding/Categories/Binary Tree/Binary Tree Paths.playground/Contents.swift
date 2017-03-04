ccbb5d7d74afb3adc2e31280a241709b21a689ae
BabyShung <babyshung8@gmail.com>
2017-03-04 17:57:51 -0500

    [TreeReview]257. Binary Tree Paths

--- SwiftCoding/Categories/Binary Tree/Binary Tree Paths.playground/Contents.swift
+++ SwiftCoding/Categories/Binary Tree/Binary Tree Paths.playground/Contents.swift
@@ -7,21 +7,19 @@ import ZHDataStructure
 /// - Returns: <#return value description#>
 
 func binaryTreePaths(_ root: TreeNode?) -> [String] {
-    guard let root = root else {
-        return []
-    }
+    guard let root = root else { return [] }
     let left = binaryTreePaths(root.left)
     let right = binaryTreePaths(root.right)
-    
+    var res = [String]()
+    for path in left {
+        res.append(String(root.val) + "->" + path)
+    }
+    for path in right {
+        res.append(String(root.val) + "->" + path)
+    }
     if left.count == 0 && right.count == 0 {
-        return [String(root.val)]
-    }
-    var res: [String] = []
-    for str in left {
-        res.append(String(root.val) + "->" + str)
-    }
-    for str in right {
-        res.append(String(root.val) + "->" + str)
+        res.append(String(root.val))
     }
     return res
 }
+
--- SwiftCoding/Practice/0304-Tree.playground/Contents.swift
+++ SwiftCoding/Practice/0304-Tree.playground/Contents.swift
@@ -49,3 +49,19 @@ private func longestConsecutiveHelper(_ root: TreeNode?) -> LongestConsecutiveTu
     return (max(left.maxLen, right.maxLen, maxFromRoot), maxFromRoot)
 }
 
+func binaryTreePaths(_ root: TreeNode?) -> [String] {
+    guard let root = root else { return [] }
+    let left = binaryTreePaths(root.left)
+    let right = binaryTreePaths(root.right)
+    var res = [String]()
+    for path in left {
+        res.append(String(root.val) + "->" + path)
+    }
+    for path in right {
+        res.append(String(root.val) + "->" + path)
+    }
+    if left.count == 0 && right.count == 0 {
+        res.append(String(root.val))
+    }
+    return res
+}
