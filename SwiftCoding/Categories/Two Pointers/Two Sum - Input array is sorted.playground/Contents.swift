521bcedaf9fe072bdd5359ef3b09dee89a6d894e
Hao Zheng <babyshung8@gmail.com>
2017-03-05 18:14:21 -0500

    [TP]167. Two Sum II - Input array is sorted

--- SwiftCoding/Categories/Two Pointers/Two Sum - Input array is sorted.playground/Contents.swift
+++ SwiftCoding/Categories/Two Pointers/Two Sum - Input array is sorted.playground/Contents.swift
@@ -1,5 +1,23 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+/// 167. Two Sum II - Input array is sorted
+/// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/?tab=Description
+/// - Parameters:
+///   - numbers: <#numbers description#>
+///   - target: <#target description#>
+/// - Returns: <#return value description#>
 
-var str = "Hello, playground"
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    guard nums.count > 0 else { return [] }
+    var start = 0, end = nums.count - 1
+    while start < end {
+        let front = nums[start], rear = nums[end]
+        if front + rear == target {
+            return [start + 1, end + 1]
+        } else if front + rear > target {
+            end -= 1
+        } else {
+            start += 1
+        }
+    }
+    return []
+}
