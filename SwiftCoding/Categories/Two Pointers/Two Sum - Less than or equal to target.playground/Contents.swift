f27a800fddd24b60630d86b8324169cbb9f2ac43
Hao Zheng <babyshung8@gmail.com>
2017-03-05 18:09:20 -0500

    [TP]Two Sum - Less than or equal to target

--- SwiftCoding/Categories/Two Pointers/Two Sum - Less than or equal to target.playground/Contents.swift
+++ SwiftCoding/Categories/Two Pointers/Two Sum - Less than or equal to target.playground/Contents.swift
@@ -1,5 +1,23 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+/// Two Sum - Less than or equal to target
+/// http://www.lintcode.com/en/problem/two-sum-less-than-or-equal-to-target/
+/// - Parameters:
+///   - nums: <#nums description#>
+///   - target: <#target description#>
+/// - Returns: <#return value description#>
 
-var str = "Hello, playground"
+func twoSum5(_ nums: [Int], _ target: Int) -> Int {
+    guard nums.count > 0 else { return 0 }
+    var nums = nums.sorted(), count = 0
+    var start = 0, end = nums.count - 1
+    while start < end {
+        let front = nums[start], rear = nums[end]
+        if front + rear <= target {
+            count += end - start
+            start += 1
+        } else {
+            end -= 1
+        }
+    }
+    return count
+}
