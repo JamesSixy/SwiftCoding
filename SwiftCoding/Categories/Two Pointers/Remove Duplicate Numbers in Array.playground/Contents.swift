872b1e33f69fb04b35735bf152665547218e0620
BabyShung <babyshung8@gmail.com>
2017-03-05 17:50:58 -0500

    [TP]26. Remove Duplicates from Sorted Array

--- SwiftCoding/Categories/Two Pointers/Remove Duplicate Numbers in Array.playground/Contents.swift
+++ SwiftCoding/Categories/Two Pointers/Remove Duplicate Numbers in Array.playground/Contents.swift
@@ -1,5 +1,18 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+/// 26. Remove Duplicates from Sorted Array
+/// https://leetcode.com/problems/remove-duplicates-from-sorted-array/?tab=Description
+/// - Parameter nums: nums
+/// - Returns: new size
+/// Two pointers start at 0 index. One prev, one cur. if same, cur keep going. Else, put cur element at prev
 
-var str = "Hello, playground"
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    guard nums.count > 0 else { return 0 }
+    var prev = 0
+    for i in stride(from: 0, to: nums.count, by: 1) {
+        if nums[i] != nums[prev] {
+            prev += 1
+            nums[prev] = nums[i]
+        }
+    }
+    return prev + 1
+}
