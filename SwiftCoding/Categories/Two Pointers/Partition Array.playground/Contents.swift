bdd2042db4953d46860bf7fe64862f73b5aa1d9f
Hao Zheng <babyshung8@gmail.com>
2017-03-05 21:47:49 -0500

    [TP]Partition Array

--- SwiftCoding/Categories/Two Pointers/Partition Array.playground/Contents.swift
+++ SwiftCoding/Categories/Two Pointers/Partition Array.playground/Contents.swift
@@ -1,5 +1,31 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+/// c
+/// http://www.lintcode.com/en/problem/partition-array/
+/// - Parameters:
+///   - nums: <#nums description#>
+///   - target: <#target description#>
+/// - Returns: <#return value description#>
 
-var str = "Hello, playground"
+func partitionArray(_ nums: [Int], _ k: Int) -> Int {
+
+    guard nums.count > 0 else { return -1 }
+    var nums = nums
+    var start = 0, end = nums.count - 1
+    while start < end {
+        while start < end && nums[start] < k {
+            start += 1
+        }
+        while start < end && nums[end] >= k {
+            end -= 1
+        }
+        if start < end {
+            let tmp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = tmp
+        }
+    }
+    if nums[start] < k {
+        return start + 1
+    }
+    return start
+}
