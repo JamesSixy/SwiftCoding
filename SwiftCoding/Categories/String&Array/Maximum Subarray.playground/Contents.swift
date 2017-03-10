09c242b4ad29933792ac4e59debeac0370cf2041
Hao Zheng <babyshung8@gmail.com>
2017-03-09 22:41:41 -0500

    53. Maximum Subarray

--- SwiftCoding/Categories/String&Array/Maximum Subarray.playground/Contents.swift
+++ SwiftCoding/Categories/String&Array/Maximum Subarray.playground/Contents.swift
@@ -5,5 +5,12 @@
 /// - Returns: <#return value description#>
 
 func maxSubArray(_ nums: [Int]) -> Int {
-    
+    guard nums.count > 0 else { return 0 }
+    var res = Int.min, sum = 0, minSum = 0
+    for num in nums {
+        sum += num
+        res = max(res, sum - minSum)
+        minSum = min(minSum, sum)
+    }
+    return res
 }
