4ebb1eb6180a44ecefe5c97449beb747441ebd45
BabyShung <babyshung8@gmail.com>
2017-03-09 23:15:25 -0500

    [PrefixSum]Subarray Sum

--- SwiftCoding/Categories/String&Array/Best Time to Buy and Sell Stock.playground/Contents.swift
+++ SwiftCoding/Categories/String&Array/Best Time to Buy and Sell Stock.playground/Contents.swift
@@ -3,7 +3,14 @@
 /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?tab=Description
 /// - Parameter prices: <#prices description#>
 /// - Returns: <#return value description#>
+/// enumerate but not DP, DP is like a 2^n, but this one is O(n^2) wrost
 
 func maxProfit(_ prices: [Int]) -> Int {
-    
+    guard prices.count > 0 else { return 0 }
+    var best = Int.min, minPrice = prices[0]
+    for sellPoint in stride(from: 1, to: prices.count, by: 1) {
+        best = max(best, prices[sellPoint] - minPrice)
+        minPrice = min(minPrice, prices[sellPoint])
+    }
+    return best
 }
--- SwiftCoding/Categories/String&Array/Subarray Sum.playground/Contents.swift
+++ SwiftCoding/Categories/String&Array/Subarray Sum.playground/Contents.swift
@@ -3,7 +3,41 @@
 /// http://www.lintcode.com/en/problem/subarray-sum/
 /// - Parameter nums: <#nums description#>
 /// - Returns: <#return value description#>
+/* 
+ Prefix sum
+ if ps[j] = ps[i], (j < i, ps[0] = -1), it means ps[i] - ps[j] = 0
+ then elements from [j + 1, i] will be the result
+ */
 
 func subarraySum(_ nums: [Int]) -> [Int] {
-    
+    guard nums.count > 0 else { return [] }
+    var dict = [Int : Int](), sum = 0
+    dict[0] = -1
+    for (i, num) in nums.enumerated() {
+        sum += num
+        if let index = dict[sum] {
+            return [index + 1, i]
+        }
+        dict[sum] = i
+    }
+    return [] //not found
 }
+
+let testCases = [
+    [-1, 2, 3, -1, -2],
+    [3, 0, -11, 9, -1, -3, 8]
+]
+
+for arr in testCases {
+    print(subarraySum(arr))
+}
+
+/*
+ [-1, 2, 3, -1, -2]
+ (0, -1)
+ sum = -1, (-1, 0)
+ sum = 1, (1, 1)
+ sum = 4, (4, 2)
+ sum = 3, (3, 3)
+ sum = 1, return [1 + 1, 4]
+ */
