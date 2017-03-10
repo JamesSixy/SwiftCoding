fada8c491c3e44580f72b598a48abe65067c0928
Hao Zheng <babyshung8@gmail.com>
2017-03-10 00:26:54 -0500

    Review code

--- SwiftCoding/Categories/String&Array/Maximum Subarray.playground/Contents.swift
+++ SwiftCoding/Categories/String&Array/Maximum Subarray.playground/Contents.swift
@@ -3,6 +3,15 @@
 /// https://leetcode.com/problems/maximum-subarray/?tab=Description
 /// - Parameter nums: <#nums description#>
 /// - Returns: <#return value description#>
+/*
+ Difference between Maximum Subarray & Subarray sum?
+ Both use prefix sum but for different purpose.
+ Maximum Subarray uses it to get the max subarray sum in each loop
+ Subarray sum wants to find a subarray that sums up to 0, 
+ so it stores prefix sum in each loop in a map, then if we find existing prefix sum
+ it means ps[i] - ps[j] = 0, so nums[j+1 ... i] will be the subarray that sums up to 0
+ 
+ */
 
 func maxSubArray(_ nums: [Int]) -> Int {
     guard nums.count > 0 else { return 0 }
@@ -24,6 +33,12 @@ for arr in testCases {
 }
 
 /**
+ for i in the loop, we sum up from nums[0 ... i]
+ and then we calculate sum - minSum, which means
+ we get a possible largest subarray sum from substraction of two prefixSums
+ res defaults to Int.min and minSum defaults to 0
+ and then get max between sub - minSum and res,
+ get min between minSum and sum
  
   0 [-2, 1, -3, 4, -1, 2, 1, -5, 4]
                [4, -1, 2, 1] = 6
