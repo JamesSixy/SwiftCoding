96440029a7903d08c339c25d436272e50201f3ae
Hao Zheng <babyshung8@gmail.com>
2017-03-10 23:30:19 -0500

    [Review]Knight SP level order solution

--- SwiftCoding/Categories/BFS/Knight Shortest Path.playground/Contents.swift
+++ SwiftCoding/Categories/BFS/Knight Shortest Path.playground/Contents.swift
@@ -1,5 +1,4 @@
 
-import ZHDataStructure
 
 /// Knight Shortest Path
 /// http://www.lintcode.com/en/problem/knight-shortest-path/
@@ -16,7 +15,9 @@ import ZHDataStructure
  3. isValid need to also check steps[nx][ny] > steps[ox][oy] + 1
  */
 
-func shortestPath(_ grid: [[Bool]], source: Point, destination: Point) -> Int {
+typealias Point = (x: Int, y: Int)
+
+func shortestPath(_ grid: [[Int]], source: Point, destination: Point) -> Int {
     guard grid.count > 0 else { return -1 }
     guard source != destination else { return 0 }
     let dx = [-2, -2, -1, -1, 1, 1, 2, 2]
@@ -44,9 +45,82 @@ private func isValid(_ lastX: Int,
                      _ lastY: Int,
                      _ x: Int,
                      _ y: Int,
-                     _ grid: [[Bool]],
+                     _ grid: [[Int]],
                      _ steps: [[Int]]) -> Bool {
     return x >= 0 && x < grid.count && y >= 0 &&
-        y < grid[0].count && !grid[x][y] &&
+        y < grid[0].count && grid[x][y] == 0 &&
         steps[x][y] > steps[lastX][lastY] + 1
+}
+
+
+/*
+ Use level order
+ */
+
+private func nlIsValid(_ grid: [[Character]], _ i: Int, _ j: Int) -> Bool {
+    return i >= 0 && i < grid.count && j >= 0 && j < grid[0].count && grid[i][j] == "1"
+}
+
+func shortestPathLevel(_ grid: [[Int]], source: Point, destination: Point) -> Int {
+    guard grid.count > 0 else { return -1 }
+    guard source != destination else { return 0 }
+    var steps = Array(repeating: Array(repeatElement(Int.max, count: grid[0].count)), count: grid.count)
+    var res = 0
+    var queue = [source]
+    steps[source.x][source.y] = 0
+    let dx = [1, 1, -1, -1, 2, 2, -2, -2]
+    let dy = [2, -2, 2, -2, 1, -1, 1, -1]
+    while !queue.isEmpty {
+        let size = queue.count
+        res += 1
+        for _ in 0 ..< size {
+            let cur = queue.removeFirst() //dequeue
+            if cur == destination {
+                return steps[cur.x][cur.y]
+            }
+            for k in 0 ..< 8 {
+                let nx = dx[k] + cur.x
+                let ny = dy[k] + cur.y
+                guard spIsValid(grid, steps, nx, ny) else { continue }
+                queue.append(Point(nx, ny))
+                steps[nx][ny] = res
+            }
+        }
+    }
+    return -1
+}
+
+private func spIsValid(_ grid: [[Int]], _ steps: [[Int]], _ i: Int, _ j: Int) -> Bool {
+    return i >= 0 && i < steps.count && j >= 0 && j < steps[0].count && steps[i][j] == Int.max && grid[i][j] == 0
+}
+
+///////
+
+let testCases = [
+    ([
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+     ], Point(2, 0), Point(2, 2)),
+    ([
+        [0,1,0],
+        [0,0,0],
+        [0,0,0]
+        ], Point(2, 0), Point(2, 2)),
+    ([
+        [0,1,0],
+        [0,0,1],
+        [0,0,0]
+        ], Point(2, 0), Point(2, 2)),
+    ([
+        [1,0,0],
+        [0,0,1],
+        [0,1,0]
+        ], Point(2, 0), Point(0, 2))
+
+]
+
+for tuple in testCases {
+    print(shortestPath(tuple.0, source: tuple.1, destination: tuple.2))
+    print(shortestPathLevel(tuple.0, source: tuple.1, destination: tuple.2))
 }
--- SwiftCoding/Practice/0310-BFS.playground/Contents.swift
+++ SwiftCoding/Practice/0310-BFS.playground/Contents.swift
@@ -1,5 +1,5 @@
 
-import ZHDataStructure
+//import ZHDataStructure
 
 //enum Numbers: String {
 //    case Zero = "0"
@@ -7,39 +7,97 @@ import ZHDataStructure
 //}
 
 typealias Point = (x: Int, y: Int)
-
-func numIslands(_ grid: [[Character]]) -> Int {
-    guard grid.count > 0 else { return 0 }
-    var res = 0, grid = grid
-    for (i, row) in grid.enumerated() {
-        for (j, _) in row.enumerated() {
-            if grid[i][j] == "1" {
-                res += 1
-                nlBFS(&grid, i, j)
-            }
-        }
-    }
-    return res
-}
-
-private func nlBFS(_ grid: inout [[Character]], _ i: Int, _ j: Int) {
-    let root: Point = (i, j)
-    var queue = [root]
-    grid[i][j] = "0"
-    let dx = [0, 0, 1, -1]
-    let dy = [1, -1, 0, 0]
-    while !queue.isEmpty {
-        let cur = queue.removeFirst() //dequeue
-        for k in 0 ..< 4 {
-            let nx = dx[k] + cur.x, ny = dy[k] + cur.y
-            if nlIsValid(grid, nx, ny) {
-                queue.append((nx, ny)) //enqueue
-                grid[nx][ny] = "0"
-            }
-        }
-    }
-}
+//
+//func numIslands(_ grid: [[Character]]) -> Int {
+//    guard grid.count > 0 else { return 0 }
+//    var res = 0, grid = grid
+//    for (i, row) in grid.enumerated() {
+//        for (j, _) in row.enumerated() {
+//            if grid[i][j] == "1" {
+//                res += 1
+//                nlBFS(&grid, i, j)
+//            }
+//        }
+//    }
+//    return res
+//}
+//
+//private func nlBFS(_ grid: inout [[Character]], _ i: Int, _ j: Int) {
+//    let root: Point = (i, j)
+//    var queue = [root]
+//    grid[i][j] = "0"
+//    let dx = [0, 0, 1, -1]
+//    let dy = [1, -1, 0, 0]
+//    while !queue.isEmpty {
+//        let cur = queue.removeFirst() //dequeue
+//        for k in 0 ..< 4 {
+//            let nx = dx[k] + cur.x, ny = dy[k] + cur.y
+//            if nlIsValid(grid, nx, ny) {
+//                queue.append((nx, ny)) //enqueue
+//                grid[nx][ny] = "0"
+//            }
+//        }
+//    }
+//}
 
 private func nlIsValid(_ grid: [[Character]], _ i: Int, _ j: Int) -> Bool {
     return i >= 0 && i < grid.count && j >= 0 && j < grid[0].count && grid[i][j] == "1"
 }
+
+func shortestPath(_ grid: [[Int]], source: Point, destination: Point) -> Int {
+    guard grid.count > 0 else { return -1 }
+    guard source != destination else { return 0 }
+    var steps = Array(repeating: Array(repeatElement(Int.max, count: grid[0].count)), count: grid.count)
+    var res = 0
+    var queue = [source]
+    steps[source.x][source.y] = 0
+    let dx = [1, 1, -1, -1, 2, 2, -2, -2]
+    let dy = [2, -2, 2, -2, 1, -1, 1, -1]
+    while !queue.isEmpty {
+        let size = queue.count
+        res += 1
+        for _ in 0 ..< size {
+            let cur = queue.removeFirst() //dequeue
+            if cur == destination {
+                return steps[cur.x][cur.y]
+            }
+            for k in 0 ..< 8 {
+                let nx = dx[k] + cur.x
+                let ny = dy[k] + cur.y
+                guard spIsValid(grid, steps, nx, ny) else { continue }
+                queue.append(Point(nx, ny))
+                steps[nx][ny] = res
+            }
+        }
+    }
+    return -1
+}
+
+private func spIsValid(_ grid: [[Int]], _ steps: [[Int]], _ i: Int, _ j: Int) -> Bool {
+    return i >= 0 && i < steps.count && j >= 0 && j < steps[0].count && steps[i][j] == Int.max && grid[i][j] == 0
+}
+
+let testCases = [
+    ([
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+        ], Point(2, 0), Point(2, 2)),
+    ([
+        [0,1,0],
+        [0,0,0],
+        [0,0,0]
+        ], Point(2, 0), Point(2, 2)),
+    ([
+        [0,1,0],
+        [0,0,1],
+        [0,0,0]
+        ], Point(2, 0), Point(2, 2))
+]
+
+for tuple in testCases {
+    print(shortestPath(tuple.0, source: tuple.1, destination: tuple.2))
+}
+
+
+
