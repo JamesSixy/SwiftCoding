ed847e325472a854b4eea85157dd5fd57be46a36
Hao Zheng <babyshung8@gmail.com>
2017-03-11 20:14:05 -0500

    adding more

---
+++ SwiftCoding/Categories/Binary Search/Maximum Number in Mountain Sequence.playground/Contents.swift
@@ -0,0 +1,8 @@
+
+/// Maximum Number in Mountain Sequence
+/// http://www.lintcode.com/en/problem/maximum-number-in-mountain-sequence/
+/// - Parameter nums: <#nums description#>
+/// - Returns: <#return value description#>
+func mountainSequence(_ nums: [Int]) -> Int {
+    
+}
---
+++ SwiftCoding/Categories/Binary Search/Search a 2D Matrix.playground/Contents.swift
@@ -0,0 +1,10 @@
+
+/// 74. Search a 2D Matrix
+/// https://leetcode.com/problems/search-a-2d-matrix/?tab=Description
+/// - Parameters:
+///   - matrix: <#matrix description#>
+///   - target: <#target description#>
+/// - Returns: <#return value description#>
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    
+}
