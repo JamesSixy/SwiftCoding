5f32524a146bfae041bfa0672c6943ff6c6b11b4
BabyShung <babyshung8@gmail.com>
2017-03-11 21:58:06 -0500

    [LL]141. Linked List Cycle

--- SwiftCoding/Categories/LinkedList/Linked List Cycle.playground/Contents.swift
+++ SwiftCoding/Categories/LinkedList/Linked List Cycle.playground/Contents.swift
@@ -6,6 +6,14 @@ import ZHDataStructure
 /// - Parameter head: <#head description#>
 /// - Returns: <#return value description#>
 
-func hasCycle(head: ListNode) -> Bool {
-    
+func hasCycle(head: ListNode?) -> Bool {
+    var slow = head, fast = head
+    while fast != nil && fast?.next != nil {
+        fast = fast?.next?.next
+        slow = slow?.next
+        if slow === fast {
+            return true
+        }
+    }
+    return false
 }
