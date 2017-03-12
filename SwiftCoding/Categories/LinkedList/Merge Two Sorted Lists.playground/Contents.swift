d1cea0bc75c2ea6ed7962fcbf29dd60e3f086e3a
BabyShung <babyshung8@gmail.com>
2017-03-11 21:52:55 -0500

    [LL]86. Partition List

--- SwiftCoding/Categories/LinkedList/Merge Two Sorted Lists.playground/Contents.swift
+++ SwiftCoding/Categories/LinkedList/Merge Two Sorted Lists.playground/Contents.swift
@@ -7,6 +7,20 @@ import ZHDataStructure
 ///   - l1: <#l1 description#>
 ///   - l2: <#l2 description#>
 /// - Returns: <#return value description#>
+
 func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-    
+    let dummy = ListNode(1)
+    var cur: ListNode? = dummy, l1 = l1, l2 = l2
+    while l1 != nil && l2 != nil {
+        if l1!.val <= l2!.val {
+            cur?.next = l1
+            l1 = l1?.next
+        } else {
+            cur?.next = l2
+            l2 = l2?.next
+        }
+        cur = cur?.next
+    }
+    cur?.next = l1 == nil ? l2 : l1
+    return dummy.next
 }
--- SwiftCoding/Categories/LinkedList/Partition List.playground/Contents.swift
+++ SwiftCoding/Categories/LinkedList/Partition List.playground/Contents.swift
@@ -7,6 +7,22 @@ import ZHDataStructure
 ///   - head: <#head description#>
 ///   - x: <#x description#>
 /// - Returns: <#return value description#>
+
 func partition(_ head: ListNode?, _ x: Int) -> ListNode? {
-    
+    let l1 = ListNode(1), l2 = ListNode(1)
+    var small: ListNode? = l1, big: ListNode? = l2
+    var cur = head
+    while cur != nil {
+        if cur!.val < x {
+            small?.next = cur
+            small = small?.next
+        } else {
+            big?.next = cur
+            big = big?.next
+        }
+        cur = cur?.next
+    }
+    big?.next = nil //last one can belong to l1
+    small?.next = l2.next
+    return l1.next
 }
