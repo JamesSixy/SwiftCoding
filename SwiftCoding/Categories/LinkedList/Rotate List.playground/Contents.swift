0d617ae32a56ec0c7fddc1023aede33ae28b9ea8
BabyShung <babyshung8@gmail.com>
2017-03-12 20:35:47 -0400

    [LL]Rotate List

--- SwiftCoding/Categories/LinkedList/Rotate List.playground/Contents.swift
+++ SwiftCoding/Categories/LinkedList/Rotate List.playground/Contents.swift
@@ -7,6 +7,36 @@ import ZHDataStructure
 ///   - head: <#head description#>
 ///   - k: <#k description#>
 /// - Returns: <#return value description#>
+
 func rotateRight(_ head: ListNode?, _ k: Int) -> ListNode? {
+    if head == nil {
+        return head
+    }
     
+    let len = getLength(head)
+    let k = k % len
+    let dummy = ListNode(0)
+    dummy.next = head
+    var head: ListNode? = dummy, tail = head
+    for _ in 0 ..< k {
+        head = head?.next
+    }
+    while head?.next != nil {
+        head = head?.next
+        tail = tail?.next
+    }
+    
+    head?.next = dummy.next
+    dummy.next = tail?.next
+    tail?.next = nil
+    return dummy.next
+}
+
+private func getLength(_ head: ListNode?) -> Int {
+    var count = 0, head = head
+    while head != nil {
+        count += 1
+        head = head?.next
+    }
+    return count
 }
