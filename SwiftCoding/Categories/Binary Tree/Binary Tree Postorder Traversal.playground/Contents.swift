36736913bfb20143c8332cdc27700d1ff5c9e241
Hao Zheng <babyshung8@gmail.com>
2017-03-13 23:29:29 -0400

    reuse code

--- SwiftCoding/Categories/Binary Tree/Binary Tree Postorder Traversal.playground/Contents.swift
+++ SwiftCoding/Categories/Binary Tree/Binary Tree Postorder Traversal.playground/Contents.swift
@@ -7,5 +7,20 @@ import ZHDataStructure
 /// - Parameter root: <#root description#>
 /// - Returns: <#return value description#>
 func postorderTraversal(_ root: TreeNode?) -> [Int] {
-
+    
+    var res = [Int]()
+    var stack = [TreeNode]()
+    var node = root
+    
+    while !stack.isEmpty || node != nil {
+        if node != nil {
+            res.insert(node!.val, at: 0)
+            stack.append(node!)
+            node = node!.right
+        } else {
+            node = stack.removeLast().left
+        }
+    }
+    
+    return res
 }
--- SwiftCoding/Categories/Two Pointers/Valid Palindrome.playground/Contents.swift
+++ SwiftCoding/Categories/Two Pointers/Valid Palindrome.playground/Contents.swift
@@ -3,7 +3,39 @@
 /// https://leetcode.com/problems/valid-palindrome/?tab=Description
 /// - Parameter s: <#s description#>
 /// - Returns: <#return value description#>
+/// O(n) time and space
+
+import UIKit
 
 func isPalindrome(_ s: String) -> Bool {
+    let chars = Array(s.lowercased().characters)
     
+    var left = 0
+    var right = chars.count - 1
+    
+    while left < right {
+        while left < right && !isAlpha(chars[left]) {
+            left += 1
+        }
+        while left < right && !isAlpha(chars[right]) {
+            right -= 1
+        }
+        
+        if chars[left] != chars[right] {
+            return false
+        } else {
+            left += 1
+            right -= 1
+        }
+    }
+    
+    return true
+}
+
+private func isAlpha(_ char: Character) -> Bool {
+    guard let char = String(char).unicodeScalars.first else {
+        fatalError("Character is invalid")
+    }
+    
+    return CharacterSet.alphanumerics.contains(char)
 }
