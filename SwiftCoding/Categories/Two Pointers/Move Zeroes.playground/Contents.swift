1d78961d2df239c3247d1388dfdc06b63feb5de4
BabyShung <babyshung8@gmail.com>
2017-03-14 11:46:52 -0400

    clean up

--- SwiftCoding/Categories/Two Pointers/Move Zeroes.playground/Contents.swift
+++ SwiftCoding/Categories/Two Pointers/Move Zeroes.playground/Contents.swift
@@ -4,16 +4,27 @@
 /// - Parameter nums: <#nums description#>
 
 func moveZeroes(_ nums: inout [Int]) {
-    guard nums.count > 0 else {
-        return
-    }
+    guard nums.count > 0 else { return }
     var last = 0
-    for i in stride(from: 0, to: nums.count, by: 1) {
+    for (i, _) in nums.enumerated() {
         if nums[i] != 0 {
             if (last != i) {
                 swap(&nums[i], &nums[last])
             }
             last += 1
         }
+    }
+}
+
+//Double loop, first round putting all none-zero at front, then set the rest zero
+func moveZeroes2(_ nums: inout [Int]) {
+    guard nums.count > 0 else { return }
+    var last = 0
+    for num in nums where num != 0 {
+        nums[last] = num
+        last += 1
+    }
+    for i in last ..< nums.count {
+        nums[i] = 0
     }
 }
