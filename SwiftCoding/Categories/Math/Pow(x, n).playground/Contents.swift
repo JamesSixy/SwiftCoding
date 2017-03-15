24f314093c564d8945ada3c914944e40e9cb59e7
Hao Zheng <babyshung8@gmail.com>
2017-03-14 21:57:33 -0400

    pow

--- SwiftCoding/Categories/Math/Pow(x, n).playground/Contents.swift
+++ SwiftCoding/Categories/Math/Pow(x, n).playground/Contents.swift
@@ -5,7 +5,41 @@
 ///   - x: <#x description#>
 ///   - n: <#n description#>
 /// - Returns: <#return value description#>
+/**
+ 50. Pow(x, n)
+ Category: [<#Description#>]
+ 
+ Question: <#Description#>
+ 
+ Link: https://leetcode.com/problems/powx-n/?tab=Description
+ Solution: https://segmentfault.com/a/1190000003756872
+ Return: <#Description#>
+ 
+ Idea: <#Description#>
+ 
+ Time: <#Description#>, Space: <#Description#>
+ 
+ */
 
 func myPow(_ x: Double, _ n: Int) -> Double {
-    
+    if n < 0 {
+        return 1 / powHelper(x, -n)
+    } else {
+        return powHelper(x, n)
+    }
+}
+
+private func powHelper(_ x: Double, _ n: Int) -> Double {
+    if n == 0 {
+        return 1.0
+    }
+    if n == 1 {
+        return x
+    }
+    let val = powHelper(x, n / 2)
+    if n % 2 == 0 {
+        return val * val
+    } else {
+        return val * val * x
+    }
 }
