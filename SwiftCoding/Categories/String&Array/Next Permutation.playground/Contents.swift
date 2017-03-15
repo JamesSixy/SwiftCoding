4491cdfbf06e9d1d8b46ccca8e37f9ea7867adac
Hao Zheng <babyshung8@gmail.com>
2017-03-14 21:47:37 -0400

    Adding questions

---
+++ SwiftCoding/Categories/String&Array/Next Permutation.playground/Contents.swift
@@ -0,0 +1,40 @@
+
+import ZHDataStructure
+import UIKit
+
+/**
+ 31. Next Permutation
+ Category: [<#Description#>]
+ 
+ Question: <#Description#>
+ 
+ Link: https://leetcode.com/problems/next-permutation/#/description
+ Solution: <#Description#>
+ Return: <#Description#>
+ 
+ Idea: <#Description#>
+ 
+ Time: <#Description#>, Space: <#Description#>
+ 
+ */
+
+func nextPermutation(_ nums: inout [Int]) {
+    
+}
+
+/**
+ TestCases: <#Description#>
+ */
+let testCases = [
+    <#Description#>
+]
+
+for <#Description#> in testCases {
+    print(<#Description#>)
+}
+
+
+
+
+
+
---
+++ SwiftCoding/Categories/String&Array/Previous Permutation.playground/Contents.swift
@@ -0,0 +1,40 @@
+
+import ZHDataStructure
+import UIKit
+
+/**
+  Previous Permutation
+ Category: [<#Description#>]
+ 
+ Question: <#Description#>
+ 
+ Link: http://www.lintcode.com/en/problem/previous-permutation/
+ Solution: http://www.jiuzhang.com/solutions/previous-permutation/
+ Return: <#Description#>
+ 
+ Idea: <#Description#>
+ 
+ Time: <#Description#>, Space: <#Description#>
+ 
+ */
+
+func previousPermutation(_ nums: inout [Int]) {
+    
+}
+
+/**
+ TestCases: <#Description#>
+ */
+let testCases = [
+    <#Description#>
+]
+
+for <#Description#> in testCases {
+    print(<#Description#>)
+}
+
+
+
+
+
+
