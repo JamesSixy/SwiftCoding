ec5912fd17a28d01b9f402ab93e0b39954fdb1d4
Hao Zheng <babyshung8@gmail.com>
2017-03-15 22:21:01 -0400

    Reverse Words in a String II

--- SwiftCoding/Categories/String&Array/Reverse Words in a String II.playground/Contents.swift
+++ SwiftCoding/Categories/String&Array/Reverse Words in a String II.playground/Contents.swift
@@ -18,52 +18,52 @@ import UIKit
  
  */
 
-//func reverseWords(_ arr: [Character]) -> String {
-//    guard arr.count > 0 else { return "" }
-//    var arr = arr
-//    reverse(&arr, 0, arr.count - 1)
-//    var last = 0, cur = 0
-//    while cur < arr.count {
-//        if arr[cur] != " " {
-//            last = cur
-//            while cur < arr.count && arr[cur] != " " {
-//                cur += 1
-//            }
-//            reverse(&arr, last, cur - 1)
-//        } else {
-//            cur += 1
-//        }
-//    }
-//    return String(arr)
-//}
-//
-//private func reverse(_ arr: inout [Character],
-//                     _ start: Int,
-//                     _ end: Int) {
-//    for i in stride(from: 0, through: (end - start + 1) / 2, by: 1) {
-//        swap(&arr, start + i, end - i)
-//    }
-//}
-//
-//func swap<T>(_ nums: inout [T], _ p: Int, _ q: Int) {
-//    (nums[p], nums[q]) = (nums[q], nums[p])
-//}
-//
-///**
-// TestCases: <#Description#>
-// */
-//let testCases = [
-//    "What day is it",
-//    "How are you",
-//    "Jump it",
-//    "Dad",
-//    ""
-//]
-//
-//for str in testCases {
-//    let arr = [Character](str.characters)
-//    print(reverseWords(arr))
-//}
+func reverseWords(_ arr: inout [Character]) {
+    guard arr.count > 0 else { return }
+    reverse(&arr, 0, arr.count - 1)
+    var last = 0, cur = 0
+    while cur < arr.count {
+        if arr[cur] != " " {
+            last = cur
+            while cur < arr.count && arr[cur] != " " {
+                cur += 1
+            }
+            reverse(&arr, last, cur - 1)
+        } else {
+            cur += 1
+        }
+    }
+}
+
+private func reverse<T>(_ nums: inout [T], _ i: Int, _ j: Int) {
+    var i = i, j = j
+    while i < j {
+        swap(&nums, i, j)
+        i += 1
+        j -= 1
+    }
+}
+
+func swap<T>(_ nums: inout [T], _ p: Int, _ q: Int) {
+    (nums[p], nums[q]) = (nums[q], nums[p])
+}
+
+/**
+ TestCases: <#Description#>
+ */
+let testCases = [
+    "What day is it",
+    "How are you",
+    "Jump it",
+    "Dad",
+    ""
+]
+
+for str in testCases {
+    var arr = [Character](str.characters)
+    reverseWords(&arr)
+    print(String(arr))
+}
 
 
 
