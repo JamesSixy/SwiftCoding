51e7ef70eab05bd4e2b30004456f9089aed14013
BabyShung <babyshung8@gmail.com>
2017-03-18 16:06:33 -0400

    Convert Binary Tree to Linked Lists by Depth

--- SwiftCoding/Categories/DFS/Convert Binary Tree to Linked Lists by Depth.playground/Contents.swift
+++ SwiftCoding/Categories/DFS/Convert Binary Tree to Linked Lists by Depth.playground/Contents.swift
@@ -1,11 +1,59 @@
 
 import ZHDataStructure
 
-/// Convert Binary Tree to Linked Lists by Depth
-/// http://www.lintcode.com/en/problem/convert-binary-tree-to-linked-lists-by-depth/
-/// http://www.jiuzhang.com/solutions/convert-binary-tree-to-linked-lists-by-depth/
-/// - Parameter root: <#root description#>
-/// - Returns: <#return value description#>
+/**
+ Convert Binary Tree to Linked Lists by Depth
+ Category: [DFS]
+ 
+ Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+ 
+ Link: http://www.lintcode.com/en/problem/convert-binary-tree-to-linked-lists-by-depth/
+ Solution: http://www.jiuzhang.com/solutions/convert-binary-tree-to-linked-lists-by-depth/
+ 
+ 通用的DFS时间复杂度计算公式:
+ O(答案个数 * 构造每个答案的时间)
+ 
+ Time: O(n), Space: O(n)
+ 
+ */
+
 func binaryTreeToLists(_ root: TreeNode?) -> [ListNode] {
-    
+    var res = [ListNode]()
+    binaryTreeToListsDFS(root, 1, &res)
+    return res
 }
+
+private func binaryTreeToListsDFS(_ root: TreeNode?,
+                                  _ depth: Int,
+                                  _ res: inout [ListNode]) {
+    guard let root = root else { return }
+    let node = ListNode(root.val)
+    if res.count < depth {
+        res.append(node)
+    } else {
+        node.next = res[depth - 1]
+        res[depth - 1] = node
+    }
+    binaryTreeToListsDFS(root.right, depth + 1, &res)
+    binaryTreeToListsDFS(root.left, depth + 1, &res)
+}
+
+let testData = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+]
+for arr in testData {
+    let root = arrayToBinaryTree(arr, 0)
+    for node in binaryTreeToLists(root) {
+        var node: ListNode? = node
+        var str = ""
+        while node != nil {
+            str = str + "\(node!.val)->"
+            node = node?.next
+        }
+        str.append("null")
+        print(str)
+    }
+}
+
+
+
--- SwiftCoding/Categories/DFS/Remove Invalid Parentheses.playground/Contents.swift
+++ SwiftCoding/Categories/DFS/Remove Invalid Parentheses.playground/Contents.swift
@@ -4,6 +4,42 @@
 /// - Parameter s: <#s description#>
 /// - Returns: <#return value description#>
 
+//DaoZhang
 func removeInvalidParentheses(_ s: String) -> [String] {
-    
+    var res = [String](), s = Array(s.characters)
+    dfs(&res, s, 0, 0, (Character("("), Character(")")))
+    return res
 }
+
+private func dfs(_ res: inout [String],
+                 _ s: [Character],
+                 _ lastI: Int,
+                 _ lastJ: Int,
+                 _ parens: (Character, Character)) {
+    var stack = 0, s = s
+    
+    for i in lastI ..< s.count {
+        if s[i] == parens.0 {
+            stack += 1
+        }
+        if s[i] == parens.1 {
+            stack -= 1
+        }
+        
+        if stack < 0 {
+            for j in lastJ ... i {
+                if s[j] == parens.1 && (j == lastJ || s[j - 1] != parens.1) {
+                    dfs(&res, Array(s[0 ..< j] + s[j + 1 ..< s.count]), i, j, parens)
+                }
+            }
+            return
+        }
+    }
+    
+    if parens.0 == "(" {
+        dfs(&res, s.reversed(), 0, 0, (Character(")"), Character("(")))
+    } else {
+        res.append(String(s.reversed()))
+    }
+}
+
