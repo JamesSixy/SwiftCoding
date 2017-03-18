7784cdf36d58743c8a660cc52703f8c385be44f8
BabyShung <babyshung8@gmail.com>
2017-03-18 11:10:38 -0400

    [Review]Combinations

--- SwiftCoding/Categories/DFS/Combination.playground/Contents.swift
+++ SwiftCoding/Categories/DFS/Combination.playground/Contents.swift
@@ -1,11 +1,18 @@
 
-
-/// 77. Combinations
-/// https://leetcode.com/problems/combinations/?tab=Description
-/// - Parameters:
-///   - n: <#n description#>
-///   - k: <#k description#>
-/// - Returns: <#return value description#>
+/**
+ 77. Combinations
+ Category: [DFS]
+ 
+ Question: Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ 
+ Link: https://leetcode.com/problems/combinations/?tab=Description
+ 
+ 通用的DFS时间复杂度计算公式:
+ O(答案个数 * 构造每个答案的时间)
+ 
+ Time: O(C(n, k)) < O(2^n), Space: O(n)
+ 
+ */
 
 func combine(_ n: Int, _ k: Int) -> [[Int]] {
     var res: [[Int]] = []
@@ -24,7 +31,7 @@ func combineHelper(_ res: inout [[Int]],
         res.append(container)
         return
     }
-    for i in stride(from: start, through: n, by: 1) {
+    for i in start ... n {
         container.append(i)
         combineHelper(&res, &container, n, k, i + 1)
         container.removeLast()
