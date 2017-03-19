d580b61ad8b3a3f0223ab6f00ce61f66240f5a76
Hao Zheng <babyshung8@gmail.com>
2017-03-19 16:23:15 -0700

    Still got some to go

---
+++ SwiftCoding/Categories/Binary Tree/Construct Binary Tree from Inorder and Postorder Traversal.playground/Contents.swift
@@ -0,0 +1,65 @@
+
+import ZHDataStructure
+import UIKit
+
+/**
+ 106. Construct Binary Tree from Inorder and Postorder Traversal
+ Category: [<#Description#>]
+ 
+ Question: Given inorder and postorder traversal of a tree, construct the binary tree.
+ 
+ Note:
+ You may assume that duplicates do not exist in the tree
+ 
+ Link: https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+ 
+ Idea: Always use the last element in postorder as root, then find that one in inorder to get left and right subtrees
+ 
+ Time: O(n), Space: O(n)
+ 
+ */
+
+func buildTree(inorder: [Int], _ postorder: [Int]) -> TreeNode? {
+    guard inorder.count > 0 && postorder.count > 0 && inorder.count == postorder.count else {
+        return nil
+    }
+    
+    return _buildHelper(inorder, 0, inorder.count - 1, postorder, 0, postorder.count - 1)
+}
+
+private func _buildHelper(inorder: [Int], _ inStart: Int, _ inEnd: Int, _ postorder: [Int], _ postStart: Int, _ postEnd: Int) -> TreeNode? {
+    guard inStart <= inEnd && postStart <= postEnd else {
+        return nil
+    }
+    
+    let root = TreeNode(postorder[postEnd])
+    
+    var mid = 0
+    for i in inStart ... inEnd where inorder[i] == root.val {
+        mid = i
+        break
+    }
+    
+    root.left = _buildHelper(inorder, inStart, mid - 1, postorder, postStart, mid - 1 - inStart + postStart)
+    root.right = _buildHelper(inorder, mid + 1, inEnd, postorder, mid - inStart + postStart, postEnd - 1)
+    
+    return root
+}
+
+
+/**
+ TestCases: <#Description#>
+ */
+let testCases = [
+    <#Description#>
+]
+
+for <#Description#> in testCases {
+    print(<#Description#>)
+}
+
+
+
+
+
+
---
+++ SwiftCoding/Categories/String&Array/Set Matrix Zeroes.playground/Contents.swift
@@ -0,0 +1,79 @@
+
+import ZHDataStructure
+import UIKit
+
+/**
+ Set Matrix Zeroes
+ Category: [<#Description#>]
+ 
+ Question: Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+ 
+ Link: https://leetcode.com/problems/set-matrix-zeroes/
+ 
+ Idea: Use first row and col to track if row and col should be set as 0, remember they should be separate from other parts of matrix
+ 
+ Time:  O(n^2), Space: O(1)
+ 
+ */
+
+func setZeroes(_ matrix: inout [[Int]]) {
+    var rowHasZero = false, colHasZero = false
+    let m = matrix.count, n = matrix[0].count
+    
+    for i in 0 ..< m where matrix[i][0] == 0 {
+        colHasZero = true
+        break
+    }
+    
+    for i in 0 ..< n where matrix[0][i] == 0 {
+        rowHasZero = true
+        break
+    }
+    
+    for i in 1 ..< m {
+        for j in 1 ..< n {
+            if matrix[i][j] == 0 {
+                matrix[0][j] = 0
+                matrix[i][0] = 0
+            }
+        }
+    }
+    
+    for i in 1 ..< m {
+        for j in 1 ..< n {
+            if matrix[0][j] == 0 || matrix[i][0] == 0 {
+                matrix[i][j] = 0
+            }
+        }
+    }
+    
+    if rowHasZero {
+        for i in 0 ..< n {
+            matrix[0][i] = 0
+        }
+    }
+    
+    if colHasZero {
+        for i in 0 ..< m {
+            matrix[i][0] = 0
+        }
+    }
+}
+
+
+/**
+ TestCases: <#Description#>
+ */
+let testCases = [
+    <#Description#>
+]
+
+for <#Description#> in testCases {
+    print(<#Description#>)
+}
+
+
+
+
+
+
