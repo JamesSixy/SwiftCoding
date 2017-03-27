c667045553d2fc02f2a683a8f3371ffd0d55c227
BabyShung <babyshung8@gmail.com>
2017-03-27 08:36:40 -0400

    242. Valid Anagram

--- SwiftCoding/Categories/String&Array/Valid Anagram.playground/Contents.swift
+++ SwiftCoding/Categories/String&Array/Valid Anagram.playground/Contents.swift
@@ -5,6 +5,20 @@
 ///   - s: <#s description#>
 ///   - t: <#t description#>
 /// - Returns: <#return value description#>
+
+/**
+ * Question Link: https://leetcode.com/problems/valid-anagram/
+ * Primary idea: Transfer string to char array and sort, compare the sort one
+ * Time Complexity: O(nlogn), Space Complexity: O(1)
+ */
+
 func isAnagram(_ s: String, _ t: String) -> Bool {
-    
+    guard s.characters.count == t.characters.count else { return false }
+    return sortStr(s) == sortStr(t)
+}
+
+private func sortStr(_ s: String) -> [Character] {
+    let sChars = [Character](s.characters)
+    sChars.sorted {$0 < $1}
+    return sChars
 }
