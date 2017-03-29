7da1a4c3a31e93f9728913ba80483e9a83b81515
BabyShung <babyshung8@gmail.com>
2017-03-28 22:00:43 -0400

    kth

--- SwiftCoding/Categories/Two Pointers/Kth Largest Element in an Array.playground/Contents.swift
+++ SwiftCoding/Categories/Two Pointers/Kth Largest Element in an Array.playground/Contents.swift
@@ -8,38 +8,49 @@
 
 //Kth largest means get (count - k + 1)th minimum
 func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
-    guard nums.count > 0 && k > 0 else { return 0 }
-    return helper(nums, 0, nums.count - 1, nums.count - k + 1)
+    guard nums.count > 0 && k > 0 && k <= nums.count else { return -1 }
+    var nums = nums, start = 0, end = nums.count - 1, index = nums.count - k
+    while start < end {
+        let pivot = partition(&nums, start, end)
+        if pivot == index {
+            return nums[pivot]
+        } else if pivot < index {
+            start = pivot + 1
+        } else {
+            end = pivot - 1
+        }
+     }
+    return nums[start]
 }
 
-private func helper(_ nums: [Int], _ l: Int, _ r: Int, _ k: Int) -> Int {
-    var nums = nums
-    if l == r {
-        return nums[l]
+private func partition(_ nums: inout [Int], _ start: Int, _ end: Int) -> Int {
+    var start = start, end = end
+    let pivot = nums[start]
+    while start < end {
+        while start < end && nums[end] >= pivot {
+            end -= 1
+        }
+        nums[start] = nums[end]
+        while start < end && nums[start] <= pivot {
+            start += 1
+        }
+        nums[end] = nums[start]
     }
-    let pos = partition(&nums, l, r)
-    if pos + 1 == k {
-        return nums[pos]
-    } else if pos + 1 < k {
-        return helper(nums, pos + 1, r, k)
-    } else {
-        return helper(nums, l, pos - 1, k)
-    }
+    nums[start] = pivot
+    return start
 }
 
-private func partition(_ nums: inout [Int], _ l: Int, _ r: Int) -> Int {
-    var l = l, r = r
-    let pivot = nums[l]
-    while l < r {
-        while l < r && nums[r] >= pivot {
-            r -= 1
-        }
-        nums[l] = nums[r]
-        while l < r && nums[l] <= pivot {
-            l += 1
-        }
-        nums[r] = nums[l]
-    }
-    nums[l] = pivot
-    return l
+private func swap<T>(_ nums: inout [T], _ i: Int, _ j: Int) {
+    (nums[i], nums[j]) = (nums[j], nums[i])
+}
+
+let testCase = [
+    ([5,4,6,2],1),
+    ([5,2,4,1,3,6,0],2),
+    ([3,2,1,3,3,3],2)
+]
+for tuple in testCase {
+    print(tuple.0)
+    print(findKthLargest(tuple.0, tuple.1))
+    print()
 }
