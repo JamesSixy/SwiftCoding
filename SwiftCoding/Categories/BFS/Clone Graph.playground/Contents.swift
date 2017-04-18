f59ce6727ac263f164551658b3bdb9a45d624338
BabyShung <babyshung8@gmail.com>
2017-04-17 22:23:54 -0400

    133. Clone Graph

--- SwiftCoding/Categories/BFS/Clone Graph.playground/Contents.swift
+++ SwiftCoding/Categories/BFS/Clone Graph.playground/Contents.swift
@@ -1,18 +1,26 @@
 
 import ZHDataStructure
 
-/// 133. Clone Graph
-/// https://leetcode.com/problems/clone-graph/?tab=Description
-/// - Parameter node: <#node description#>
-/// - Returns: cloned
-/// 1.clone nodes 2.clone edges
+/**
+ 133. Clone Graph
+ Category: [BFS]
+ 
+ Question: Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+ 
+ Link: https://leetcode.com/problems/clone-graph/?tab=Description
+ 
+ Idea: 1.clone nodes 2.clone edges
+ 
+ Time: O(n), Space: O(n)
+ 
+ */
 
 func cloneGraph(_ node: UndirectedGraphNode) -> UndirectedGraphNode {
     let dict = cloneNodesDict(node)
     
-    for (old, new) in dict {
-        for neighbor in old.neighbors {
-            new.neighbors.append(dict[neighbor]!)
+    for (cur, copy) in dict {
+        for neighbor in cur.neighbors {
+            copy.neighbors.append(dict[neighbor]!)
         }
     }
     return dict[node]!
@@ -20,7 +28,6 @@ func cloneGraph(_ node: UndirectedGraphNode) -> UndirectedGraphNode {
 
 private func cloneNodesDict(_ node: UndirectedGraphNode) -> [UndirectedGraphNode : UndirectedGraphNode] {
     var dict = [UndirectedGraphNode : UndirectedGraphNode]()
-    
     var queue = [node], visited = Set<UndirectedGraphNode>()
     visited.insert(node)
     
