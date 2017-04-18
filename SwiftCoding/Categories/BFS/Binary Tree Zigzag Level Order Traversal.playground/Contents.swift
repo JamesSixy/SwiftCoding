6b3f79c0b589c035d1027c53eddd1f51d0434bdb
BabyShung <babyshung8@gmail.com>
2017-04-17 22:06:28 -0400

    103. Binary Tree Zigzag Level Order Traversal

--- SwiftCoding/Categories/BFS/Binary Tree Zigzag Level Order Traversal.playground/Contents.swift
+++ SwiftCoding/Categories/BFS/Binary Tree Zigzag Level Order Traversal.playground/Contents.swift
@@ -1,10 +1,65 @@
 
 import ZHDataStructure
 
-/// 103. Binary Tree Zigzag Level Order Traversal
-/// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/?tab=Description
-/// - Parameter root: <#root description#>
-/// - Returns: <#return value description#>
+/**
+ 103. Binary Tree Zigzag Level Order Traversal
+ Category: [BFS]
+ 
+ Question: Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+ 
+ For example:
+ Given binary tree [3,9,20,null,null,15,7],
+ 3
+ / \
+ 9  20
+ /  \
+ 15   7
+ return its zigzag level order traversal as:
+ [
+ [3],
+ [20,9],
+ [15,7]
+ ]
+ 
+ Link: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/?tab=Description
+ 
+ Idea: use a queue and a flag, level traversal
+ 
+ Time: O(n), Space: O(n)
+ 
+ */
+
 func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+    var res: [[Int]] = []
+    guard let root = root else { return res }
     
+    var queue = [root]
+    var flag = false
+    while !queue.isEmpty {
+        var size = queue.count
+        var arr: [Int] = []
+        for i in 0 ..< size {
+            let cur = queue.removeFirst()
+            arr.append(cur.val)
+            if let left = cur.left {
+                queue.append(left)
+            }
+            if let right = cur.right {
+                queue.append(right)
+            }
+        }
+        let shouldReverse = flag
+        flag = !flag
+        res.append(shouldReverse ? arr.reversed() : arr)
+    }
+    return res
 }
+
+
+
+
+
+
+
+
+
---
+++ SwiftCoding/Practice/0417.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+import ZHDataStructure
+import UIKit
+
+
+
+
+
+
