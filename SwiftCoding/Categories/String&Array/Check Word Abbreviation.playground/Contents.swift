d7c811c1c8b13f48b6770badf8094e327030d5ae
BabyShung <babyshung8@gmail.com>
2017-05-02 21:45:37 -0400

    Check Word Abbreviation prep

---
+++ SwiftCoding/Categories/String&Array/Check Word Abbreviation.playground/Contents.swift
@@ -0,0 +1,50 @@
+
+import ZHDataStructure
+import UIKit
+
+/**
+ Check Word Abbreviation
+ 
+ Link: http://lintcode.com/en/problem/check-word-abbreviation/
+ 
+ Category: [<#Description#>]
+ 
+ Question: Given a non-empty string word and an abbreviation abbr, return whether the string matches with the given abbreviation.
+ 
+ A string such as "word" contains only the following valid abbreviations:
+ 
+ ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+ Notice
+ 
+ Notice that only the above abbreviations are valid abbreviations of the string word. Any other string is not a valid abbreviation of word.
+ 
+ Idea: <#Description#>
+ 
+ Time: O(<#Description#>), Space: O(<#Description#>)
+ 
+ */
+
+func <#Description#>(<#Description#>) {
+    <#Description#>
+}
+
+private func <#Description#>(<#Description#>) -> <#Description#> {
+    
+}
+
+/**
+ TestCases: <#Description#>
+ */
+let testCases = [
+    <#Description#>
+]
+
+for <#Description#> in testCases {
+    print(<#Description#>)
+}
+
+
+
+
+
+
