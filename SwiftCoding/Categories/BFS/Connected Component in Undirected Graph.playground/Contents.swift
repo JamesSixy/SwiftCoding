aeb0fb9f9d7163775f3551f7cb0596b2520fdb1d
BabyShung <babyshung8@gmail.com>
2017-05-13 14:39:05 -0400

    [BFS]Connected Component in Undirected Graph

--- SwiftCoding/Categories/BFS/Connected Component in Undirected Graph.playground/Contents.swift
+++ SwiftCoding/Categories/BFS/Connected Component in Undirected Graph.playground/Contents.swift
@@ -1,10 +1,64 @@
 
 import ZHDataStructure
 
-/// Connected Component in Undirected Graph
-/// http://www.lintcode.com/en/problem/connected-component-in-undirected-graph/
-/// - Parameter nodes: <#nodes description#>
-/// - Returns: <#return value description#>
+/**
+ Connected Component in Undirected Graph
+ 
+ Category: [BFS]
+ 
+ Question: Find the number connected component in the undirected graph. Each node in the graph contains a label and a list of its neighbors. (a connected component (or just component) of an undirected graph is a subgraph in which any two vertices are connected to each other by paths, and which is connected to no additional vertices in the supergraph.)
+ 
+ Notice
+ Each connected component should sort by label.
+ 
+ Example
+ Given graph:
+ 
+ A-----B   C
+ \     |   |
+  \    |   |
+   \   |   |
+    \  |   |
+      D    E
+ Return {A,B,D}, {C,E}. Since there are two connected component which is {A,B,D}, {C,E}
+ 
+ Link: http://www.lintcode.com/en/problem/connected-component-in-undirected-graph/
+ 
+ Time: O(n), Space: O(n)
+ 
+ */
+
 func connectedSet(_ nodes: [UndirectedGraphNode]) -> [[Int]] {
-    
+    var res: [[Int]] = []
+    guard nodes.count > 0 else { return res }
+    var visited = Set<UndirectedGraphNode>()
+    for node in nodes {
+        guard !visited.contains(node) else { continue }
+        ccBFS(node, visited, &res)
+    }
+    return res
 }
+
+private func ccBFS(_ node: UndirectedGraphNode,
+                   _ visited: Set<UndirectedGraphNode>,
+                   _ res: inout [[Int]]) {
+    var queue = [node], component = [Int]()
+    visited.insert(node)
+
+    while !queue.isEmpty {
+        let cur = queue.removeFirst()
+        component.append(cur.label)
+        for neighbor in cur.neighbors {
+            if !visited.contains(neighbor) {
+                queue.append(neighbor)
+                visited.insert(neighbor)
+            }
+        }
+    }
+    res.append(component.sorted())
+}
+
+
+
+
+
