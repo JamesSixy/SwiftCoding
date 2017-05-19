2a925ed9ef5404306d377c37a23b028851f107b8
BabyShung <babyshung8@gmail.com>
2017-05-18 22:58:08 -0400

    tmp

--- SwiftCoding/Categories/BFS/Island Perimeter.playground/Contents.swift
+++ SwiftCoding/Categories/BFS/Island Perimeter.playground/Contents.swift
@@ -1,8 +1,16 @@
 
-/// 463. Island Perimeter
-/// https://leetcode.com/problems/island-perimeter/?tab=Description
-/// - Parameter grid: <#grid description#>
-/// - Returns: <#return value description#>
+/**
+ 463. Island Perimeter
+ 
+ Category: [BFS]
+ 
+ Question: You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water. Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells). The island doesn't have "lakes" (water inside that isn't connected to the water around the island). One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+ 
+ Link: https://leetcode.com/problems/island-perimeter/?tab=Description
+ 
+ Time: , Space:
+ 
+ */
 
 func islandPerimeter(_ grid: [[Int]]) -> Int {
     var islands = 0, neighbors = 0
